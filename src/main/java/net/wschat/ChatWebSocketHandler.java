d2235a5d47ea50c5ecbfa559d543d752d4fc5f21
achim <weight.of.fire@googlemail.com>
2012-02-25 13:00:37 +0100

    First version of server (as simple echo-server)

---
+++ src/main/java/net/wschat/ChatWebSocketHandler.java
@@ -0,0 +1,48 @@
+package net.wschat;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.eclipse.jetty.websocket.WebSocket;
+import org.eclipse.jetty.websocket.WebSocket.OnTextMessage;
+import org.eclipse.jetty.websocket.WebSocketHandler;
+
+public class ChatWebSocketHandler extends WebSocketHandler {
+
+	public WebSocket doWebSocketConnect(HttpServletRequest request, String protocol) {
+		return new ChatWebSocket();
+	}
+}
+
+class ChatWebSocket implements OnTextMessage {
+	private Connection conn;
+
+	@Override
+	public void onClose(int closeCode, String msg) {
+		System.out.println("ChatWebSocket.onClose()");
+		close();
+	}
+
+	@Override
+	public void onOpen(Connection conn) {
+		System.out.println("ChatWebSocket.onOpen()");
+		this.conn = conn;
+	}
+
+	@Override
+	public void onMessage(String msg) {
+		System.out.println("ChatWebSocket.onMessage()");
+		try {
+			conn.sendMessage("You said: " + msg);
+		} catch (IOException e) {
+			e.printStackTrace();
+			close();
+		}
+
+	}
+
+	private void close() {
+		conn.disconnect();
+	}
+}
--- src/main/java/net/wschat/Main.java
+++ src/main/java/net/wschat/Main.java
@@ -1,6 +1,8 @@
 package net.wschat;
 
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.DefaultHandler;
+import org.eclipse.jetty.websocket.WebSocketHandler;
 
 /**
  * 
@@ -8,12 +10,12 @@ import org.eclipse.jetty.server.Server;
  */
 public class Main {
 	public static void main(String[] args) throws Exception {
-//	    final WebSocketHandler wsHandler = new ChatWebSocketHandler();
-//	    wsHandler.setHandler( new DefaultHandler() );
+		final WebSocketHandler wsHandler = new ChatWebSocketHandler();
+		wsHandler.setHandler(new DefaultHandler());
 
-		final Server server = new Server( 8080 );
-//	    server.setHandler( wsHandler );
+		final Server server = new Server(8080);
+		server.setHandler(wsHandler);
 		server.start();
-		server.join();		
+		server.join();
 	}
 }
